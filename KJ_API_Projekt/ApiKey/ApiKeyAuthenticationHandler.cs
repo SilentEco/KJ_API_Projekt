46f02dc6aef14c1e5a85a61aecd102d5a8bcc74b
Kristopher Kram <kram.kristopher@gmail.com>
2021-04-30 16:20:42 +0200

    Foreach för att kolla keys

--- KJ_API_Projekt/ApiKey/ApiKeyAuthenticationHandler.cs
+++ KJ_API_Projekt/ApiKey/ApiKeyAuthenticationHandler.cs
@@ -7,21 +7,27 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using KJ_API_Projekt.model;
+using KJ_API_Projekt.data;
+using Microsoft.EntityFrameworkCore;
 
 namespace KJ_API_Projekt.ApiKey
 {
     public class ApiKeyAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
     {
+        private readonly ApplicationDbContext _context;
         public ApiKeyAuthenticationHandler(
             IOptionsMonitor<AuthenticationSchemeOptions> options,
             ILoggerFactory logger,
             UrlEncoder encoder,
-            ISystemClock clock
+            ISystemClock clock,
+            ApplicationDbContext context
+
             
             )
             : base(options, logger, encoder, clock)
         {
-            
+            _context = context;
         }
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
@@ -33,21 +39,35 @@ namespace KJ_API_Projekt.ApiKey
             if (token == null)
                 return AuthenticateResult.Fail("Invalid Authorization Header");
 
+            List<ApiToken> ApiKeys = _context.ApiTokens.Include(m => m.User).ToList();
 
-            string testToken = "DemoKey";
-            if (token == testToken)
+            foreach (var key in ApiKeys)
             {
+                if (token == key.Value) 
+                {
+                    var identity = new ClaimsIdentity(Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
 
-                var identity = new ClaimsIdentity(Scheme.Name);
-                var principal = new ClaimsPrincipal(identity);
-                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
 
-                return AuthenticateResult.Success(ticket);
+                }
             }
-            else
-            {
-                return AuthenticateResult.Fail("FAIL!!!");
-            }
+            return AuthenticateResult.Fail("fail");
+           // string testToken = ApiKeys[1].Value;
+           // if (token == testToken)
+           // {
+           //
+           //     var identity = new ClaimsIdentity(Scheme.Name);
+           //     var principal = new ClaimsPrincipal(identity);
+           //     var ticket = new AuthenticationTicket(principal, Scheme.Name);
+           //
+           //     return AuthenticateResult.Success(ticket);
+           // }
+           // else
+           // {
+           //     return AuthenticateResult.Fail("FAIL!!!");
+           // }
 
         }
 

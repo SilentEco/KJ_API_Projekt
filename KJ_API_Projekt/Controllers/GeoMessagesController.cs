e57b0bb4b77d177b4e82157ae457c7b68b177a5d
JakobLarsson <jakob.larsson@live.se>
2021-05-11 10:40:45 +0200

    DTOs för get v2 (bortagning av id)

--- KJ_API_Projekt/Controllers/GeoMessagesController.cs
+++ KJ_API_Projekt/Controllers/GeoMessagesController.cs
@@ -96,20 +96,56 @@ namespace KJ_API_Projekt.Controllers
 
             [HttpGet("[action]/{id}")]
 
-            public async Task<ActionResult<GeoMessagesV2>> GetgeoMessages(int id)
+            public async Task<ActionResult<v2GetDTO>> GetgeoMessages(int id)
             {
-                return await _context.geoMessagesV2.Include(a => a.Message).FirstOrDefaultAsync(o => o.Id == id);
+                var geoDB = await _context.geoMessagesV2.Include(a => a.Message).FirstOrDefaultAsync(o => o.Id == id);
+
+                v2GetDTO geoDTO = new v2GetDTO()
+                {
+                    latitude = geoDB.latitude,
+                    longitude = geoDB.longitude,
+                    Message = new v2MessageDTO()
+                    {
+                        Author = geoDB.Message.Author,
+                        Body = geoDB.Message.Body,
+                        Title = geoDB.Message.Title
+
+                    }
+                };
+
+                return geoDTO; 
                // return await _context.geoMessagesV2.Include(a => a.Message).ToListAsync();
             }
 
             [HttpGet("[action]")]
 
-            public async Task<ActionResult<IEnumerable<GeoMessagesV2>>> GetgeoMessages(double minLon, double maxLon, double minLat, double maxLat)
+            public async Task<ActionResult<IEnumerable<v2GetDTO>>> GetgeoMessages(double minLon, double maxLon, double minLat, double maxLat)
             {
-                
-                return await _context.geoMessagesV2.Include(a => a.Message).Where(
-                    o => (o.longitude <= maxLon && o.longitude >= minLon) && (o.latitude <= maxLat && o.latitude >= minLat) )
+                var geoDB = await _context.geoMessagesV2.Include(a => a.Message).Where(
+                    o => (o.longitude <= maxLon && o.longitude >= minLon) && (o.latitude <= maxLat && o.latitude >= minLat))
                     .ToListAsync();
+
+                List<v2GetDTO> listGeoDTO = new List<v2GetDTO>();
+                foreach(var item in geoDB)
+                {
+                    v2GetDTO geoDTO = new v2GetDTO()
+                    {
+                        latitude = item.latitude,
+                        longitude = item.longitude,
+                        Message = new v2MessageDTO()
+                        {
+                            Author = item.Message.Author,
+                            Body = item.Message.Body,
+                            Title = item.Message.Title
+
+                        }
+                    };
+
+                    listGeoDTO.Add(geoDTO);
+                }
+
+
+                return listGeoDTO;
             }
 
 
@@ -123,6 +159,32 @@ namespace KJ_API_Projekt.Controllers
 
                 return CreatedAtAction("GetGeoMessages", new { id = geoMessages.Id }, geoMessages);
             }
+
+            #region DTO classer
+
+            public class v2MessageDTO
+            {
+                public string Title { get; set; }
+                public string Body { get; set; }
+                public string Author { get; set; }
+            }
+            public class v2GetDTO
+            {
+                
+
+                public v2MessageDTO Message { get; set; }
+
+                public double longitude { get; set; }
+
+                public double latitude { get; set; }
+
+
+            }
+
+
+            #endregion
+
+
 
         }
 
